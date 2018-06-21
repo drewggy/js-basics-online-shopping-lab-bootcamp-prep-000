cd561bf1579d440f341fc0e42a86110be9af18df
Andrew Sommer <sommer.drew@gmail.com>
2018-06-21 17:05:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function viewCart() {
 
   var outString = "In your cart, you have ";
   for (var i = 0; i < cart.length; i++){
-    console.log(`Loop ${i} - length is ${cart.length}`);
+//    console.log(`Loop ${i} - length is ${cart.length}`);
     if (i > 0 && i < cart.length-1) outString += ", ";
     if (cart.length > 1 && i === cart.length-1) outString += " and ";
     outString += `${cart[i].itemName} at ${cart[i].itemPrice}`;
