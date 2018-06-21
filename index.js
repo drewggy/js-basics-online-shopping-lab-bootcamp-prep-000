56eea93578ab618c9012fc3b78bdc6b237af9921
Andrew Sommer <sommer.drew@gmail.com>
2018-06-21 18:51:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,7 +58,10 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if(!cardNumber) return "Sorry, we don't have a credit card on file for you.";
-  return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+
+  var outString = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+  cart.length = 0;
+  return outString;
 }
 
 
